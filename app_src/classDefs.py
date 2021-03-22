f6d0caabad83a25c6129088f86ca4b0ee68bcf5e
JhadotAnimesh <jha.animesh01@gmail.com>
2021-03-22 16:03:26 +0530

    hardcoded data gen

---
+++ app_src/classDefs.py
@@ -0,0 +1,49 @@
+import datetime
+
+
+class Location:
+    def __init__(self, lat, long):
+        self.lat = lat
+        self.long = long
+
+
+class Payment:
+    def __init__(self, upiid):
+        self.upiid = upiid
+
+
+class User:
+    def __init__(self, name, phnum, paymentdetails):
+        self.name = name
+        self.phnum = phnum
+        self.paymentdetails = paymentdetails
+
+
+class Driver:
+    def __init__(self, name, car, rating, kyd, carno):
+        self.name = name
+        self.car = car
+        self.rating = rating
+        self.kyd = kyd
+        self.carnumber = carno
+
+
+class Ride:
+    def __init__(self, t, fro, usr, driv, fr, com, datetime, rfc=False, expay=0):
+        self.to = t
+        self.fro = fro
+        self.user = usr
+        self.driver = driv
+        self.fare = fr
+        self.completed = com
+        self.date_time = datetime
+        self.isrideforcause = rfc
+        self.extrapay = expay
+
+
+class CauseDriver(Driver):
+    def __init__(self, name, car, rating, kyd, carno, needinf, amtneeded, amtraised):
+        super().__init__(name, car, rating, kyd, carno)
+        self.needinfo = needinf
+        self.amountraised = amtraised
+        self.amountneeded = amtneeded
