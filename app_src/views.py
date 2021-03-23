c991dcc13a2471abbeca938b3d5726a5fe8656d2
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-03-23 18:26:40 +0530

    Basic testing

--- app_src/views.py
+++ app_src/views.py
@@ -1,5 +1,6 @@
+from flask.helpers import make_response
 from app_src import app
-from flask import render_template
+from flask import render_template, jsonify, make_response
 import json
 import random
 
@@ -14,3 +15,9 @@ def temp():
 @app.route("/temp2.html")
 def temp2():
     return render_template("temp2.html")
+
+@app.route("/getdata")
+def getdata():
+    with open("data.json") as f:
+        data = json.load(f)
+    return make_response(jsonify(data))
