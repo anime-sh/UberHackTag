3c2c363964e00957048cd070f72d6a9c2ff8b7aa
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-03-23 15:31:01 +0530

    Added views
    New background, is the quality good?

--- app_src/views.py
+++ app_src/views.py
@@ -6,3 +6,11 @@ import random
 @app.route("/")
 def index():
     return render_template("home.html")
+
+@app.route("/temp.html")
+def temp():
+    return render_template("temp.html")
+
+@app.route("/temp2.html")
+def temp2():
+    return render_template("temp2.html")
