fc9870e684dbe9029f0e662fc1cc3a912749fd9f
NehaDalmia <52661386+NehaDalmia@users.noreply.github.com>
2021-03-23 22:12:11 +0530

    Update views.py

--- app_src/views.py
+++ app_src/views.py
@@ -1,4 +1,5 @@
 from flask.helpers import make_response
+from flask import request, redirect
 from app_src import app
 from flask import render_template, jsonify, make_response
 import json
@@ -24,17 +25,21 @@ def temp2():
 #         data = json.load(f)
 #     return make_response(jsonify(data))
 
-@app.route("/sign-up", methods=['GET', 'POST'])
-def register_for_cause():
-    return render_template("/sign_up.html")
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
     if request.method == "POST":
+
         req = request.form
+        print(type(req['file']))
         missing = list()
+
         for k, v in req.items():
             if v == "":
                 missing.append(k)
+
         if missing:
             feedback = f"Missing fields for {', '.join(missing)}"
             return render_template("/sign_up.html", feedback=feedback)
+
         return redirect(request.url)
-    return render_template("temp2.html")
+    return render_template("/sign_up.html")
